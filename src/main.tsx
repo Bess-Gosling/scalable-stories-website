ab8cf278514ab76e724abc55dc844a3d21d9e2f4
Bess Gosling <bess.gosling@outlook.com>
2025-04-05 12:05:46 +1000

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/scalable-stories-website">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
