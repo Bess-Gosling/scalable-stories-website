f8d16b5dd24e4bd335357e37828eddb8626201a3
Bess Gosling <bess.gosling@outlook.com>
2025-04-05 11:43:49 +1000

    test4

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
-import './index.css';
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter basename="/scalable-stories-website">
     <App />
   </BrowserRouter>
