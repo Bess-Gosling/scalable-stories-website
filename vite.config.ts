f5abad045e21ef7b2760914edce8bca02784670a
Bess Gosling <bess.gosling@outlook.com>
2025-04-05 15:09:40 +1000

    test

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     },
   },
   define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(supabaseUrl),
-    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(supabaseAnonKey),
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://nbmjitoxmrklflyvdbkv.supabase.co'),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ibWppdG94bXJrbGZseXZkYmt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4MTA4OTIsImV4cCI6MjA1OTM4Njg5Mn0.E7YhWmPbO6-asCJo2GTOtUOn-sUmii-AOqKYYwRaznQ'),
   },
 });
