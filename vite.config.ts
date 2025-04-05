02664fa154458f9d05fd7b8de956479f8d70ad78
Bess Gosling <bess.gosling@outlook.com>
2025-04-05 14:15:02 +1000

    Changed some files to support Github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/scalable-stories-website/", // 👈 this must match your GitHub repo name exactly
   server: {
     host: "::",
     port: 8080,
