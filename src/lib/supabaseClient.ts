cb9ad09b353b61b56cf35617b1012773242d83a2
Bess Gosling <bess.gosling@outlook.com>
2025-04-05 14:32:12 +1000

    Staged conflict

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
@@ -7,24 +5,3 @@ export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 );
-=======
-// lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
->>>>>>> 02664fa154458f9d05fd7b8de956479f8d70ad78
-=======
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Supabase env vars are missing.");
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
->>>>>>> cd93de08b8273b741cac3c8fcde4396f72d14ee6
